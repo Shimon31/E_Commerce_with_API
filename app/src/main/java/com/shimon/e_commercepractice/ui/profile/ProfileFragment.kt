a5a10855fb5ab1ac2ca3c920aa67a758db3e0fd3
Shimon <shimonbhuiya02@gmail.com>
2025-02-04 19:38:19 +0600

    ProfileFragment Start

--- app/src/main/java/com/shimon/e_commercepractice/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/shimon/e_commercepractice/ui/profile/ProfileFragment.kt
@@ -2,15 +2,24 @@ package com.shimon.e_commercepractice.ui.profile
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.shimon.e_commercepractice.base.baseFragment
 import com.shimon.e_commercepractice.databinding.FragmentProfileBinding
 
 
 class ProfileFragment : baseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate) {
 
+    val viewModel : ProfileViewModel by viewModels()
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel.profile()
+        viewModel.profileResponse.observe(viewLifecycleOwner){
+
+        }
+
 
     }
 
---
+++ app/src/main/java/com/shimon/e_commercepractice/ui/profile/ProfileViewModel.kt
@@ -0,0 +1,32 @@
+package com.shimon.e_commercepractice.ui.profile
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.shimon.e_commercepractice.Services.UserService
+import com.shimon.e_commercepractice.data.model.Login.RequestLogin
+import com.shimon.e_commercepractice.data.model.Login.ResponseLogin
+import com.shimon.e_commercepractice.data.model.profile.ResponseProfile
+import com.shimon.e_commercepractice.data.repo.AuthRepo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor(private val repo: UserService) : ViewModel() {
+
+
+    private var _response = MutableLiveData<Response<ResponseProfile>>()
+    var profileResponse: LiveData<Response<ResponseProfile>> = _response
+
+
+    fun profile() {
+        viewModelScope.launch {
+            _response.postValue(repo.profile())
+        }
+
+    }
+
+}
